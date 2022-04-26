529e1879c722d7a30db2386e3780f89063eaf762
Aaroel <aaronhernandez57@aragon.unam.mx>
2022-04-26 00:09:03 -0500

    Iniciando proyecto

---
+++ JavaApplication11/src/javaapplication11/Computadora.java
@@ -0,0 +1,14 @@
+
+package javaapplication11;
+
+
+public class Computadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
